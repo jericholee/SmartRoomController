519633a3cf024ca758fb8c36e285d605ccd3cc4d
Jericho Ortiz <jericho1990@outlook.com>
2021-10-28 14:49:15 -0600

    Completed code to get one PIR sensor working that writes reaction on the OLED screen while lighting up a LED when motion is detected.

---
+++ PIR_Test_Code/PIR_Test_Code.ino
@@ -0,0 +1,79 @@
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+
+#define SCREEN_WIDTH 128 
+#define SCREEN_HEIGHT 64
+
+#define OLED_RESET     4 // Reset pin # (or -1 if sharing Arduino reset pin)
+#define SCREEN_ADDRESS 0x3C ///< See datasheet for Address; 0x3D for 128x64, 0x3C for 128x32
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
+
+
+
+const int PIR = 14; //pin the PIR is connected to 
+const int LED = 5;
+
+
+void setup() {
+  pinMode(LED, OUTPUT);
+  pinMode(PIR, INPUT);
+  Serial.begin(9600);
+  if(!display.begin(SSD1306_SWITCHCAPVCC, SCREEN_ADDRESS)) {
+  Serial.println(F("SSD1306 allocation failed"));
+  for(;;); // Don't proceed, loop forever
+}
+  testdrawstyles();
+ }
+
+
+void loop() {
+  if(digitalRead(PIR) == HIGH) {
+    digitalWrite(LED, HIGH);
+//    Serial.println("!Presence Detected!");
+    delay(100);
+    display.clearDisplay();
+    display.setTextSize(1);
+    display.setTextColor(SSD1306_WHITE);
+    display.setCursor(0,0);
+    display.setRotation(0);
+    display.println(F("!Presence Detected!"));
+    display.setTextColor(SSD1306_BLACK, SSD1306_WHITE); // Draw 'inverse' text
+    display.setTextSize(2);             // Draw 2X-scale text
+    display.setTextColor(SSD1306_WHITE);
+    display.display();
+    delay(200); 
+  }
+  else {
+    digitalWrite(LED, LOW);
+//    Serial.println(".:Presence faded:.");
+    delay(100);
+    display.clearDisplay();
+    display.setTextSize(1);
+    display.setTextColor(SSD1306_WHITE);
+    display.setCursor(0,0);
+    display.setRotation(0);
+    display.println(F(".:Presence faded:."));
+    display.setTextColor(SSD1306_BLACK, SSD1306_WHITE); // Draw 'inverse' text
+    display.setTextSize(2);             // Draw 2X-scale text
+    display.setTextColor(SSD1306_WHITE);
+    display.display();
+    delay(200); 
+    delay(100);
+  }
+}
+
+void testdrawstyles(void) {
+ display.clearDisplay();
+ display.setTextSize(1);
+ display.setTextColor(SSD1306_WHITE);
+ display.setCursor(0,0);
+ display.setRotation(0);
+ display.println(F("Hello, Jupiter"));
+ display.setTextColor(SSD1306_BLACK, SSD1306_WHITE); // Draw 'inverse' text
+ display.setTextSize(2);             // Draw 2X-scale text
+ display.setTextColor(SSD1306_WHITE);
+ display.display();
+ delay(200); 
+
+}
